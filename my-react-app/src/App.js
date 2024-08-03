ee3109d5afe2b1022125fa38106042e1a46eef84
michaelx0281 <69123340+michaelx0281@users.noreply.github.com>
2024-08-03 19:33:03 -0400

    added minor things and now trying to deal with the errors

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState,useEffect} from 'react';
+import axios from 'axios'
 
 function App() {
   const [data, setData] = useState('');
--- my-react-node-app/server.js
+++ my-react-node-app/server.js
@@ -8,14 +8,18 @@ var corsOptions = {
 };
 
 //review what all of this means :sob: my memory has declined severely
-app.get('/api/data', (req, res) =>{
+app.get('/', (req, res) =>{
     //API LOGIC
-    const data = {message: 'i forgot how this worked'};
-    res.json(data);
+    const data = {message : 'i forgot how this worked'};
+    res.json(data.message);
 });
+
+app.get("/:universalURL", (req, res) => {
+    res.send("404 URL NOT FOUND");
+ });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
